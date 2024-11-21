607b110b0335f2ca1bc25e6306fd6a79edb92613
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-11-20 22:24:54 -0500

    attempt wasm fix

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 import topLevelAwait from 'vite-plugin-top-level-await';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-	plugins: [wasm(), topLevelAwait(), sveltekit()],
-	build: {
-		target: 'esnext',
-		modulePreload: {
-			polyfill: true
-		}
+	plugins: [
+		sveltekit(),
+		wasm(),
+		topLevelAwait(),
+		nodePolyfills({
+			globals: { Buffer: true, global: true },
+			protocolImports: true
+		})
+	],
+	define: {
+		global: 'globalThis'
 	}
 });
